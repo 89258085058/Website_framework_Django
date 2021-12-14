ba2dc63af7f36c5482ff90f5bc09839d3eb1c427
89258085058 <86528556+89258085058@users.noreply.github.com>
2021-12-14 21:52:24 +0300

    добавил новости

--- fitstsite/fitstsite/settings.py
+++ fitstsite/fitstsite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'news',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- fitstsite/main/views.py
+++ fitstsite/main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница!'
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
